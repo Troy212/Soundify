cb0821c6b1c60d88f47d0758ab678ee71a447bdd
Srinjoy Das <162009749+Troy212@users.noreply.github.com>
2024-12-17 01:09:18 +0530

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,60 +1,59 @@
-
-
 import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import useAuthModal from "@/hooks/useAuthModal";
-import { useEffect } from "react";
-
-
+import { useEffect, useState } from "react";
 
 const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const router = useRouter();
     const { session } = useSessionContext();
-    const { onClose, isOpen} = useAuthModal();
+    const { onClose, isOpen } = useAuthModal();
+    const [isProvidersWorking, setIsProvidersWorking] = useState(true);
 
     useEffect(() => {
         if (session) {
-            router.refresh();
+            router.refresh(); // Refresh the page to reflect login status
             onClose();
         }
     }, [session, router, onClose]);
 
-  const onChange = (open: boolean) => {
-    if (!open) {
-      onClose();
-    }
-  };
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    };
 
-  return (
-    <Modal
-      title="Welcome back"
-      description="Login to your account ( Login With Email , Providers Under Maintainance) "
-      isOpen={isOpen}
-      onChange={onChange}
-    >
-      <Auth
-        theme="dark"
-        magicLink
-        providers={["google", "discord", "github"]}
-        supabaseClient={supabaseClient}
-        appearance={{
-          theme: ThemeSupa,
-          variables: {
-            default: {
-              colors: {
-                brand: '#404040',
-                brandAccent: '#ff0000',
-              },
-            },
-          },
-        }}
-      />
-    </Modal>
-  );
+    return (
+        <Modal
+            title="Welcome back"
+            description={`Login to your account ${
+                isProvidersWorking ? "" : "(Providers Under Maintenance)"
+            }`}
+            isOpen={isOpen}
+            onChange={onChange}
+        >
+            <Auth
+                theme="dark"
+                magicLink
+                providers={isProvidersWorking ? ["", "discord", "github"] : []}
+                supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: "#404040",
+                                brandAccent: "#ff0000",
+                            },
+                        },
+                    },
+                }}
+            />
+        </Modal>
+    );
 };
 
 export default AuthModal;
